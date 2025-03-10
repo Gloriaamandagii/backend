5eff9979bcf9b242af40eb42e07a713e27546925
Gloriaamandagii <glorriiaamandagii@gmail.com>
2025-03-10 09:52:25 +0800

    add exercise 5

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
-const http = require("http");
-const { hello, greetings } = require("./helloWorld");
-const moment = require("moment");
 const express = require("express");
 const morgan = require("morgan");
-const errorhandler = require("errorhandler");
-const app = express();
-const routers = require("./routers");
 const path = require("path");
 const cors = require("cors");
+const routers = require("./routers");
 
-//Middleware
-const log = (req, res, next) => {};
+const app = express();
 
+// Middleware
 app.use(morgan("tiny"));
-// app.use(errorhandler);
-
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 app.use(
   cors({
     origin: "http://127.0.0.1:5500",
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 
-//Routing
+// Routing
 app.use(routers);
-app.use(express.static(path.join(__dirname, "public")));
-//Middleware untuk 404
-app.use((req, res, next) => {
+
+// Middleware untuk 404
+app.use((req, res) => {
   res.status(404).json({
     status: "error",
     message: "resource tidak ditemukan",
+  });
+});
+
+// Middleware untuk error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    status: "error",
+    message: "terjadi kesalahan pada server",
   });
 });
 
