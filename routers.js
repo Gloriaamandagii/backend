1e822806cea85b5ce4d2f0afa4e2b3e9d85298e6
Gloriaamandagii <glorriiaamandagii@gmail.com>
2025-04-21 09:24:53 +0800

    add mongoose

--- routers.js
+++ routers.js
@@ -1,18 +1,74 @@
 const express = require("express");
 const routers = express.Router();
 const path = require("path");
+const fs = require("fs");
+const multer = require("multer");
+// const client = require("./mongodb");
+// const ObjectId = require("mongodb").ObjectId;
+
+require("./mongoose");
+const Users = require("./User");
+
+const imageFilter = (req, file, cb) => {
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+    return cb(null, false);
+  }
+  cb(null, true);
+};
+
+const upload = multer({ dest: "public", fileFilter: imageFilter });
 
 // Routing
+// Get all users
+routers.get("/users", async (req, res) => {
+  const users = await Users.find();
+  res.json({
+    status: "success",
+    message: "list users",
+    data: users,
+  });
+});
+
+// Get single user
+// routers.get("/users/:id", async (req, res) => {
+//   try {
+//     const db = client.db("latihan");
+//     const user = await db.collection("users").findOne({
+//       _id: new ObjectId(req.params.id),
+//     });
+//     res.status(200).json({
+//       status: "success",
+//       message: "single user",
+//       data: user,
+//     });
+//   } catch (error) {
+//     res.json({
+//       status: "error",
+//     });
+//   }
+// });
+
+routers.post("/upload", upload.single("file"), (req, res) => {
+  const file = req.file;
+  if (file) {
+    const target = path.join(__dirname, "public", file.originalname);
+    fs.renameSync(file.path, target); //rename file agar sama dengan original file name
+    res.send("file berhasil diupload");
+  } else {
+    res.send("file gagal diupload");
+  }
+});
+
 routers.get("/download", (req, res) => {
   const filename = "dummy.png";
-  res.download(path.join(__dirname, "/download", filename), "dummpy-photo.png");
+  res.download(path.join(__dirname, "/download", filename), "dummy-photo.png");
 });
 
 routers.post("/login", (req, res) => {
   const { username, password } = req.body;
   res.status(200).json({
     status: "success",
-    message: "Login Page",
+    message: "Login page",
     data: {
       username: username,
       password: password,
@@ -21,6 +77,13 @@ routers.post("/login", (req, res) => {
 });
 routers.get("/", (req, res) => res.send("Hello World"));
 routers.get("/about", (req, res) =>
+  res.status(200).json({
+    status: "success",
+    message: "About page",
+    data: [],
+  })
+);
+routers.put("/about", (req, res) =>
   res.status(200).json({
     status: "success",
     message: "About page",
