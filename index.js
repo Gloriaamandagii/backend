3e6b76caf699c927f4e9508cd6cfbe6d31110279
Gloriaamandagii <glorriiaamandagii@gmail.com>
2025-04-21 09:29:26 +0800

    add mongoose

--- index.js
+++ index.js
@@ -3,22 +3,35 @@ const { hello, greetings } = require("./helloWorld");
 const moment = require("moment");
 const express = require("express");
 const morgan = require("morgan");
-const errorhandler = require("errorhandler");
+// const errorhandler = require("errorhandler");
 const app = express();
 const routers = require("./routers");
 const path = require("path");
+const cors = require("cors");
 
 //Middleware
-const log = (req, res, next) => {};
+const log = (req, res, next) => {
+  console.log(
+    moment().format("h:mm:ss a") + " " + req.originalUrl + " " + req.ip
+  );
+  next();
+};
 
 app.use(morgan("tiny"));
 // app.use(errorhandler);
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500",
+    methods: ["GET", "PUT"],
+  })
+);
+
 //Routing
 app.use(routers);
-app.use(express.static(path.join(__dirname, "public")));
+
 //Middleware untuk 404
 app.use((req, res, next) => {
   res.status(404).json({
